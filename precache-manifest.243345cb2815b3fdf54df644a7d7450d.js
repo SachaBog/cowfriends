37f5c9a400b5a0c1eac3a28210d17e19911b9834
Sacha <48031121+SachaBog@users.noreply.github.com>
2020-05-22 18:45:21 +0200

    Updates

---
+++ precache-manifest.243345cb2815b3fdf54df644a7d7450d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da838e0d75ee45f62021e4604ebd7928",
+    "url": "/cowfriends/index.html"
+  },
+  {
+    "revision": "d522c3f7d2bfe48e823f",
+    "url": "/cowfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fbbc4f58f07463e881dd",
+    "url": "/cowfriends/static/css/main.811bda8b.chunk.css"
+  },
+  {
+    "revision": "d522c3f7d2bfe48e823f",
+    "url": "/cowfriends/static/js/2.f825feb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cowfriends/static/js/2.f825feb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbbc4f58f07463e881dd",
+    "url": "/cowfriends/static/js/main.63db9e6f.chunk.js"
+  },
+  {
+    "revision": "607690e3a8fd92d085ad",
+    "url": "/cowfriends/static/js/runtime-main.83a052bd.js"
+  },
+  {
+    "revision": "27e827bd227cf081fc940fb1cc9cf41e",
+    "url": "/cowfriends/static/media/Tommie.27e827bd.jpg"
+  },
+  {
+    "revision": "59028438f8301562353da6508640bdf7",
+    "url": "/cowfriends/static/media/anna.59028438.jpg"
+  },
+  {
+    "revision": "e6bd32b48dd8e0284285dab59d3fdb2e",
+    "url": "/cowfriends/static/media/billie.e6bd32b4.jpg"
+  },
+  {
+    "revision": "7e16646f91d8e7f160cf6e97a0b844c3",
+    "url": "/cowfriends/static/media/cow.7e16646f.woff"
+  },
+  {
+    "revision": "a7e0992e9c675399f8dfad5a01415013",
+    "url": "/cowfriends/static/media/ellie.a7e0992e.jpg"
+  },
+  {
+    "revision": "21db2e3e844661b3f9e03678517e9014",
+    "url": "/cowfriends/static/media/kokosnoot.21db2e3e.jpg"
+  },
+  {
+    "revision": "738cb587a8943c36a29658d88a2bf4e9",
+    "url": "/cowfriends/static/media/madeliefje.738cb587.jpg"
+  },
+  {
+    "revision": "4b749e9005f6b5a3d36b6d442523b549",
+    "url": "/cowfriends/static/media/pieter.4b749e90.jpg"
+  },
+  {
+    "revision": "34ca5e26d7b113013d83f4388b887cd3",
+    "url": "/cowfriends/static/media/verlepte koe.34ca5e26.jpg"
+  },
+  {
+    "revision": "21dab777d471be522e866874d731d923",
+    "url": "/cowfriends/static/media/zilveren koe 1.21dab777.jpg"
+  },
+  {
+    "revision": "b96344f6c542fe9103708dc674e91a54",
+    "url": "/cowfriends/static/media/zilveren koe 2.b96344f6.jpg"
+  }
+]);
